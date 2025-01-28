547aa64139af3cd1a0a97025cfc28f920fa6ecc5
eduardo92005-debug <eduardo92005@gmail.com>
2025-01-28 12:46:23 -0300

    upd

--- src/main.py
+++ src/main.py
@@ -2,30 +2,43 @@ import numpy as np
 
 BASE_ADDRESS = 0x80000000  # Endereço base inicial no arquivo `.hex`
 MEMORY_SIZE = 1024 * 1024    # 32 KiB de memória
+TEXT = '''
+6F 00 00 0A 6F 00 40 08 6F 00 00 08 6F 00 C0 07
+6F 00 80 07 6F 00 40 07 6F 00 00 07 6F 00 C0 06
+6F 00 80 06 6F 00 40 06 6F 00 00 06 6F 00 C0 05
+6F 00 80 05 00 00 00 00 00 00 00 00 00 00 00 00
+17 05 00 00 13 05 05 08 97 05 00 00 93 85 85 07
+23 20 05 00 13 05 45 00 E3 4C B5 FE 67 80 00 00
+13 01 01 FF 23 22 A1 00 13 05 00 02 B7 05 02 00
+93 85 65 02 23 20 B1 00 B3 05 20 00 EF 00 40 01
+13 01 01 01 6F 00 00 00 13 05 10 00 6F F0 5F FD
+13 10 F0 01 73 00 10 00 13 50 70 40 67 80 00 00
+17 81 00 00 13 01 01 F6 EF F0 9F F9 EF 00 C0 00
+6F F0 1F FB 00 00 00 00 13 05 00 00 67 80 00 00
+'''
 
+def load_hex_to_memory_from_text(TEXT, mem, base_address):
+    lines = TEXT.strip().splitlines()
+    current_address = base_address
 
-def load_hex_to_memory_from_file(filename, mem, base_address):
-    with open(filename, "r") as file:
-        lines = file.readlines()
-        current_address = base_address
+    for line in lines:
+        line = line.strip()
+        if line.startswith("@"):
+            # Ignora linhas com "@"
+            continue
+        else:
+            # Converte os bytes para inteiros e carrega na memória
+            bytes_data = [int(byte, 16) for byte in line.split()]
+            for byte in bytes_data:
+                relative_address = current_address - base_address
+                if 0 <= relative_address < len(mem):
+                    mem[relative_address] = byte
+                    current_address += 1
+                else:
+                    raise IndexError(
+                        f"Endereço {current_address:08X} fora dos limites."
+                    )
 
-        for line in lines:
-            line = line.strip()
-            if line.startswith("@"):
-                # Ignora linhas com "@"
-                continue
-            else:
-                # Converte os bytes para inteiros e carrega na memória
-                bytes_data = [int(byte, 16) for byte in line.split()]
-                for byte in bytes_data:
-                    relative_address = current_address - base_address
-                    if 0 <= relative_address < len(mem):
-                        mem[relative_address] = byte
-                        current_address += 1
-                    else:
-                        raise IndexError(
-                            f"Endereço {current_address:08X} fora dos limites."
-                        )
 
 
 def main():
@@ -51,7 +64,7 @@ def main():
     mem = np.zeros(MEMORY_SIZE, dtype=np.uint8)
     
     # Carregando dados de um arquivo hex para a memória
-    load_hex_to_memory_from_file("../info/1_empty.hex.txt", mem, MEMORY_SIZE)
+    load_hex_to_memory_from_text(TEXT, mem, MEMORY_SIZE)
     
     # Preenchendo memória com instruções de teste
     
@@ -81,16 +94,14 @@ def main():
             (((instruction >> 20) & 1) << 10) |
             ((instruction >> 21) & 0x3FF)
         )
-        print(f"Processing instruction at 0x{pc:08x}: opcode=0x{opcode:02x}, instruction=0x{instruction:08x}")
+        print(f"Processing instruction at 0x{pc:08x}: opcode={opcode:08b}, instruction=0x{instruction:08x}")
         # Processando instruções
-        if opcode == 0b0010011:  # Tipo I (ex: slli)
-            if funct3 == 0b001 and funct7 == 0b0000000:  # slli
-                data = x[rs1] << uimm
-                print(f"0x{pc:08x}: slli   {x_label[rd]},{x_label[rs1]},{uimm}  "
-                      f"{x_label[rd]}=0x{x[rs1]:08x}<<{uimm}=0x{data:08x}")
-                if rd != 0:
-                    x[rd] = data
-        
+        if opcode == 0b0010011 and (funct3 == 0b001 and funct7 == 0b0000000):  # Tipo I (ex: slli)  # slli
+            data = x[rs1] << uimm
+            print(f"0x{pc:08x}: slli   {x_label[rd]},{x_label[rs1]},{uimm}  "
+                    f"{x_label[rd]}=0x{x[rs1]:08x}<<{uimm}=0x{data:08x}")
+            if rd != 0:
+                x[rd] = data
         elif opcode == 0b1110011:  # Tipo I (ex: ebreak)
             if funct3 == 0b000 and imm == 1:  # ebreak
                 print(f"0x{pc:08x}: ebreak")
@@ -102,7 +113,6 @@ def main():
                 )
                 if previous == 0x01f01013 and next_inst == 0x40705013:
                     run = False
-        
         elif opcode == 0b1101111:  # Tipo J (ex: jal)
             #imm20 = (instruction >> 12) & 0xFFFFF
             simm = imm20 if (imm20 >> 20) == 0 else (0xFFF00000 | imm20)
@@ -134,8 +144,6 @@ def main():
 
             # Incrementa o PC para a próxima instrução
             #pc += 4
-
-
         elif opcode == 0b0010011:  # Tipo I (addi)
             # Funct3 define qual operação do tipo I está sendo executada
             funct3 = (instruction >> 12) & 0b111
@@ -150,7 +158,7 @@ def main():
                 result = x[rs1] + imm12  # Soma o valor do registrador rs1 com o imediato
                 x[rd] = result & 0xFFFFFFFF  # Armazena o resultado (ajustado para 32 bits)
                 
-                print(f"0x{pc:08x}: addi   {x_label[rd]},{x_label[rs1]},{imm12}  "
+                print(f"0x{pc:08x}: addi   {x_label[rd]},{x_label[rs1]},{imm12:08x}  "
                     f"{x_label[rd]}=0x{result:08x},{x_label[rs1]}=0x{x[rs1]:08x}")
                 
                 pc = pc + 4  # Incrementa o PC para a próxima instrução
